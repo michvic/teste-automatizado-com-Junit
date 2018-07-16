a4b25cdd69f5bf981a1a1db57c8939d600e14e44
Michel VIctor <michelvictor.doc@gmail.com>
2018-07-16 15:55:43 -0300

    iniciando projeto

---
+++ calculadora/teste/calculadora/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
